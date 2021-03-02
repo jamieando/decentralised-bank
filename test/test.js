d3c70264800102651992f76bdc5ec0e87e92bfb8
jamieando <jamie_anderson81@hotmail.com>
2021-03-02 12:32:27 +1100

    Finished deploy file to pass over minting role to dbank and started DBank.sol file

--- test/test.js
+++ test/test.js
@@ -20,11 +20,11 @@ contract('dBank', ([deployer, user]) => {
   describe('testing token contract...', () => {
     describe('success', () => {
       it('checking token name', async () => {
-        expect(await token.name()).to.be.eq('Decentralized Bank Currency')
+        expect(await token.name()).to.be.eq('Novus Token')
       })
 
       it('checking token symbol', async () => {
-        expect(await token.symbol()).to.be.eq('DBC')
+        expect(await token.symbol()).to.be.eq('NOV')
       })
 
       it('checking token initial total supply', async () => {
